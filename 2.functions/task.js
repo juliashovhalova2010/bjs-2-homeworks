fc0bbf3a62ce1e126fb3562e891b2034b45f5e88
juliashovhalova2010 <juliashovhalova2010@yandex.ru>
2021-09-13 20:54:54 +0300

    origin

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,45 +1,31 @@
 // Задание 1
-`use strict`
-  function getArrayParams(arr) {
-    let min = Infinity;
-    let max = -Infinity;
-    let sum = 0;
-    let avg = 0;
-    for (let i = 0; i < arr.length; i++) {
-      sum = sum + arr[i];
-      if (arr[i] > max) {
-        max = arr[i];
-      }
-      if (arr[i] < min) {
-        min = arr[i];
-      }
-    }
-    avg = parseFloat((sum / arr.length).toFixed(2));
-    return { min:min, max:max, avg:avg };
-  };
+function getArrayParams(arr) {
+  let min,max,sum,avg;
+
+  // Ваш код
+
+  return { min:min, max:max, avg:avg };
+}
 
 // Задание 2
 function worker(arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
-  }
+  let sum;
+
+  // Ваш код
+
   return sum;
 }
 
-
 function makeWork(arrOfArr, func) {
-  let max = -Infinity;
-  for (let i = 0; i < arrOfArr.length; i++) {
-    let arrSum = func(arrOfArr[i]);
-    if (arrSum > max) {
-      max = arrSum;
-    }
-  }
-  return max;
+  let max
+
+  // Ваш кода
+  // for ...
+  
+  return max
 }
 
 // Задание 3
 function worker2(arr) {
-  return getArrayParams(arr).max - getArrayParams(arr).min;
+  // Ваш код
 }
